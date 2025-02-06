21a2a8266fbc01da9519c020edae5cc9229e9e65
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-02-06 21:15:20 +0200

    lending on TS

---
+++ positivus/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/navbar'
+import Banner from './Components/Banner'
+import CompanyLogo from './Components/CompanyLogo'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <CompanyLogo />
+    </>
+  )
+}
+
+export default App
---
+++ positivus/src/data.ts
@@ -0,0 +1,16 @@
+export const navLinks = [
+  { id: 1, href: '#sevices', text: 'services' },
+  { id: 2, href: '#working-process', text: 'working process' },
+  { id: 3, href: '#team', text: 'team' },
+  { id: 4, href: '#testimonials', text: 'testimonials' },
+  { id: 5, href: '#contact', text: 'contact us' },
+]
+
+export const companyLogo = [
+  { id: 1, img: '../src/assets/Company logo-1.png', alt: 'dribble' },
+  { id: 2, img: '../src/assets/Company logo-2.png', alt: 'hubspot' },
+  { id: 3, img: '../src/assets/Company logo-3.png', alt: 'notion' },
+  { id: 4, img: '../src/assets/Company logo-4.png', alt: 'netflix' },
+  { id: 5, img: '../src/assets/Company logo-5.png', alt: 'zoom' },
+  { id: 6, img: '../src/assets/Company logo.png', alt: 'amazon' },
+]
