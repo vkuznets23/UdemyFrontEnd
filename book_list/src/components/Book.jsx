86044b5e73aef14a151e590b5080d2b1f78f63f2
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-02-06 11:05:07 +0200

    almost done react fundamentals part

--- book_list/src/components/Book.jsx
+++ book_list/src/components/Book.jsx
@@ -1,7 +1,7 @@
 import '../styles/index.css'
 import {books} from '../books'
 
-const Book = ({image, alt, title, author, id, getBook, index}) => {
+const Book = ({image, alt, title, author, index}) => {
     const h4Style = {
         color: '#617d98',
         fontSize: '0.75rem',
@@ -13,24 +13,18 @@ const Book = ({image, alt, title, author, id, getBook, index}) => {
             <img src={image} alt={alt}/>
             <h2> {title} </h2>
             <h4 style={h4Style}> {author.charAt(0).toUpperCase() + author.slice(1)} </h4>
-            <button onClick={() => getBook(id)}>click</button>
             <span className='number'>{index + 1}</span>
         </div>
     )
 };
 
 const BookList = () => {
-    const getBook = (id) => {
-        const book = books.find(book => book.id === id )
-        console.log(book.title)
-    }
-    
     return (
         <div className='booklist'>
             {/* option 1: */}
             {books.map((book, index) => {
                 return (
-                    <Book {...book} key={book.id} getBook={getBook} index={index}/>
+                    <Book {...book} key={book.id} index={index}/>
                 )
             })
             // option 2:
