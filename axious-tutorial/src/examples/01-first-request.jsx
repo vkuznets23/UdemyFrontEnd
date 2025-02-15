b2cf0718a6b47756de859879657148f24479e0fd
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-02-15 17:12:08 +0200

    axios

---
+++ axious-tutorial/src/examples/01-first-request.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+// limit, if 429 wait for 15 min and try again
+const url = 'https://www.course-api.com/react-store-products'
+
+const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios(url)
+      const data = response.data
+      console.log(data)
+    } catch (err) {
+      console.log(err.response)
+    }
+  }
+  useEffect(() => {
+    fetchData()
+    console.log('first axios request')
+  }, [])
+
+  return <h2 className="text-center">first request</h2>
+}
+export default FirstRequest
