8312f5ba4d9678e21f1f36a627bf06ca60fe099a
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-27 18:47:48 +0300

    redux theory part

---
+++ redux-tutorial/starter/src/App.js
@@ -0,0 +1,12 @@
+import CartContainer from './components/CartContainer'
+import NavBar from './components/Navbar'
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      <CartContainer />
+    </main>
+  )
+}
+export default App
---
+++ redux-tutorial/starter/src/features/cart/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+import cartItems from '../../cartItems'
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [] //mutate state cos of Immer library
+    },
+  },
+})
+
+// console.log(cartSlice)
+
+export default cartSlice.reducer
+export const { clearCart } = cartSlice.actions
