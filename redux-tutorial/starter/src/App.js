b5197be250b26cc511d36a1ff398028fbf24ddfc
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-04-29 09:48:53 +0300

    finished reducer

--- redux-tutorial/starter/src/App.js
+++ redux-tutorial/starter/src/App.js
@@ -1,17 +1,19 @@
 import { useEffect } from 'react'
-import CartContainer from './components/CartContainer'
-import NavBar from './components/Navbar'
+import { CartContainer, NavBar, Modal } from './components/'
 import { useDispatch, useSelector } from 'react-redux'
 import { calculateTotals } from './features/cart/cartSlice'
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(calculateTotals())
   }, [dispatch, cartItems])
   return (
     <main>
+      {isOpen && <Modal />}
       <NavBar />
       <CartContainer />
     </main>
---
+++ redux-tutorial/starter/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Modal } from './Modal'
+export { default as CartContainer } from './CartContainer'
+export { default as CartItem } from './CartItem'
+export { default as NavBar } from './Navbar'
